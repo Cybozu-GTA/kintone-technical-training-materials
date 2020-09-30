d71633472053a743d4ccd641de30ffa33a8a76fa
Tomoko Miyake <t.smile297327@gmail.com>
2020-09-30 18:32:30 +0900

    change variable name

--- JavaScript API/changeBgColor.js
+++ JavaScript API/changeBgColor.js
@@ -7,25 +7,25 @@
     var backgroundColor;
     var fontWeight;
     switch (value) {
-    case 'Qualification':
-      backgroundColor = '#FED101';
-      break;
-    case 'Evaluation':
-      backgroundColor = '#BFFF50';
-      break;
-    case 'Negotiation':
-      backgroundColor = '#34FFF5';
-      break;
-    case 'Closed-Won':
-    case 'Contract':
-      backgroundColor = '#F9B1FB';
-      fontWeight = 'bold';
-      break;
-    case 'Closed-Lost':
-      backgroundColor = '#C6C3C3';
-      break;
-    default:
-      break;
+      case 'Qualification':
+        backgroundColor = '#FED101';
+        break;
+      case 'Evaluation':
+        backgroundColor = '#BFFF50';
+        break;
+      case 'Negotiation':
+        backgroundColor = '#34FFF5';
+        break;
+      case 'Closed-Won':
+      case 'Contract':
+        backgroundColor = '#F9B1FB';
+        fontWeight = 'bold';
+        break;
+      case 'Closed-Lost':
+        backgroundColor = '#C6C3C3';
+        break;
+      default:
+        break;
     }
 
     if (backgroundColor) {
@@ -38,19 +38,19 @@
 
   kintone.events.on('app.record.detail.show', function(event) {
     var record = event.record;
-    var value = record.Sales_Stage.value;
-    var element = kintone.app.record.getFieldElement('Sales_Stage');
-    changeSalesStageFieldColor({element: element, value: value});
+    var salesStageValue = record.Sales_Stage.value;
+    var salesStageElement = kintone.app.record.getFieldElement('Sales_Stage');
+    changeSalesStageFieldColor({element: salesStageElement, value: salesStageValue});
     return event;
   });
 
   kintone.events.on('app.record.index.show', function(event) {
     var records = event.records;
-    var elements = kintone.app.getFieldElements('Sales_Stage');
+    var salesStageElements = kintone.app.getFieldElements('Sales_Stage');
     for (var i = 0; i < records.length; i++) {
-      var value = record.Sales_Stage.value;
-      var element = elements[i];
-      changeSalesStageFieldColor({element: element, value: value});
+      var salesStageValue = record.Sales_Stage.value;
+      var salesStageElement = salesStageElements[i];
+      changeSalesStageFieldColor({element: salesStageElement, value: salesStageValue});
     }
     return event;
   });
