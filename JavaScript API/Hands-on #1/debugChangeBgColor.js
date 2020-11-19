670abe092ce36be5d73056d6234a6dbe10ac106a
TomokoMiyake <t.smile297327@gmail.com>
2020-11-19 18:50:47 +0900

    add debug file for Hands-on #1.

---
+++ JavaScript API/Hands-on #1/debugChangeBgColor.js
@@ -0,0 +1,18 @@
+(function() {
+  'use strict';
+
+  var changeSalesStageFieldColor = function(params) {
+    // add operation here
+  };
+
+  kintone.events.on('app.record.detail.show', function(event) {
+    // add operation here
+    return event;
+  });
+
+  kintone.events.on('app.record.index.show', function(event) {
+    // add operation here
+    return event;
+  });
+
+})();
